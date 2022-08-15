91966b00270b7e6c70fbe58eb94cf9c5329d795b
MisakaMikoto128 <liuyuanlins@outlook.com>
2022-08-15 18:38:34 +0800

    修复HAL库HAL_PCD_IRQHandler bug,UVC正常运行

---
+++ MDK-ARM/RTE/_UVC_H750/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'UVC_H750' 
+ * Target:  'UVC_H750' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
